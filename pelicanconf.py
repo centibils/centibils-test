0635d20ab6eec1b8c83bc1583b214bc4ef2d6c93
Tobias Brandt <Tobias.Brandt@gmail.com>
2017-02-04 12:25:22 +0200

    Adds pelican skeleton structure

---
+++ pelicanconf.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Tobias Brandt'
+SITENAME = u'SAFA 2017 Presentation'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Africa/Johannesburg'
+
+DEFAULT_LANG = u'English'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+#THEME = "C:\\Users\\Tobias\\Documents\\Python Scripts\\pelican-themes\\bootlex"
+#THEME = '../pelican-themes/pelican-blue'
+THEME = '../flasky'
