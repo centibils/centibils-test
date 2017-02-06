abc931f06e956b23e34635deaa4376a6b7a7e5b4
Tobias Brandt <Tobias.Brandt@gmail.com>
2017-02-06 15:23:52 +0200

    Adds --noekill file and changes ghp-import branch to master

--- fabfile.py
+++ fabfile.py
@@ -21,7 +21,7 @@ env.cloudfiles_api_key = 'my_rackspace_api_key'
 env.cloudfiles_container = 'my_cloudfiles_container'
 
 # Github Pages configuration
-env.github_pages_branch = "gh-pages"
+env.github_pages_branch = "master"
 
 # Port for `serve`
 PORT = 8000
@@ -89,4 +89,4 @@ def publish():
 def gh_pages():
     """Publish to GitHub Pages"""
     rebuild()
-    local("ghp-import -b {github_pages_branch} {deploy_path} -p".format(**env))
+    local("ghp-import -n -b {github_pages_branch} {deploy_path} -p".format(**env))
